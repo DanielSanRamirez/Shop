e57a75d9cef8e2262e927ef4a00269ab5f32b46f
DanielSanRamirez <danielsanramirez@hotmail.com>
2021-07-13 08:57:33 -0500

    Seed DB

--- Shop.Web/Startup.cs
+++ Shop.Web/Startup.cs
@@ -29,6 +29,9 @@ namespace Shop.Web
             {
                 cfg.UseSqlServer(this.Configuration.GetConnectionString("DefaultConnection"));
             });
+
+            services.AddTransient<SeedDb>();
+
             services.AddControllersWithViews();
         }
 
